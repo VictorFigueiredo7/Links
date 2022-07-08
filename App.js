bdaab60db39a46c8a67f38de71460cf10bb0ff5c
Victor Figueiredo <108833142+VictorFigueiredo7@users.noreply.github.com>
2022-07-08 14:01:30 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import './style.css'
+
+function App() {
+  return (
+    <body>
+      <div className="container">
+        <img className='avatar' src="https://www.github.com/victorfigueiredo7.png" alt="imagem de Victor Figueiredo" />
+        <h1>Victor Figueiredo</h1>
+        <p>@victor_figueiredo7</p>
+        <ul>
+          <li>
+            <a href="https://www.github.com/victorfigueiredo7" target="_blank">🐱GITHUB</a>
+          </li>
+          <li>
+            <a href="https://discord.com/channels/994329487560953886/994329487560953889" target="_blank">🎮DISCORD</a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/victor_figueiredo7/" target="_blank">📷INSTAGRAM</a>
+          </li>
+        </ul>
+        <footer>Espero que 💜<a href='' target="_blank">gostem</a></footer>
+      </div>
+    </body>
+  );
+}
+
+export default App;
